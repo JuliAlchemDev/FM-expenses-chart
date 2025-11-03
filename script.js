fa8d77aa03d9e3d0892342d6e1a16d294cdd9952
JuliAlchemDev <julialchem@gmail.com>
2025-11-03 09:52:19 +0100

    feat: add js and header styles

---
+++ script.js
@@ -0,0 +1,44 @@
+const barChart = document.querySelector('.bar-chart');
+
+const getData = async () => {
+    const res = await fetch('./data.json');
+    const data  = await res.json();
+    return data
+}
+
+const init = async () => {
+    const spendingInfo = await getData();
+    const amounts = spendingInfo.map(spending => spending.amount);
+    const maxAmount = Math.max(...amounts);
+    barChart.innerHTML = '';
+    
+    spendingInfo.forEach(({day, amount}) => {
+        
+        const li = document.createElement('li');
+        li.classList.add('bar-chart__day');
+
+        const tooltip = document.createElement('span')
+        tooltip.classList.add('tooltip-details');
+        tooltip.textContent = `$${amount}`
+       
+
+        const bar = document.createElement('span');
+        bar.classList.add('bar-chart__bar');
+        bar.style.height = `${amount * 0.15}rem`;
+
+       
+
+        if(amount === maxAmount){
+            bar.style.backgroundColor = 'var(--primary-blue-300)'
+            
+        }
+
+        li.append(tooltip);
+        li.append(bar)
+        li.append(day);
+        barChart.appendChild(li);
+    });
+    
+} 
+
+init();
